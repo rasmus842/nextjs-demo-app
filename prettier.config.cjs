246f4ed57dddbb5ade155af4e86e13bbd502adac
Rasmus Zalite <rasmuszalite@gmail.com>
2023-07-29 19:43:24 +0300

    prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false
+};
+
+module.exports = config;
